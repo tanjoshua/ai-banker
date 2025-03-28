d4f0cf1434708727ac914a7de70658779003388d
josh <jtanjoshua@gmail.com>
2025-03-28 22:13:28 +0800

    cleaning up db issues

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,10 +6,10 @@ console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 export default {
     schema: ['./src/lib/db/schema.ts', './src/db/schema/*'],
-    out: './drizzle',
+    out: './src/lib/db/migrations',
     dialect: 'postgresql',
     dbCredentials: {
-        connectionString: process.env.DATABASE_URL!
+        url: process.env.DATABASE_URL!
     },
     verbose: true,
     strict: true
--- src/db/schema/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './files';
-// ... any other schema exports 
